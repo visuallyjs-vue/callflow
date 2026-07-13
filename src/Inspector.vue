ec1e3aa820475cdd1d6b431d3a446aa97862300b
Simon Porritt <simon@jsplumbtoolkit.com>
2026-07-13 12:21:16 +1000

    updated to use new v-model inspector support

--- src/Inspector.vue
+++ src/Inspector.vue
@@ -1,48 +1,43 @@
 <script setup lang="ts">
-import { ref, inject } from "vue"
-import { Base, isNode, isPort, Vertex } from "@visuallyjs/browser-ui"
+import { ref, inject, computed } from "vue"
+import { isNode, isPort, Vertex } from "@visuallyjs/browser-ui"
 import { PROPERTY_TEXT, PROPERTY_URL, TYPE_CONDITIONS, PROPERTY_NUMBER, TYPE_CALL_FORWARD, TYPE_PLAY_AUDIO, TYPE_REQUEST, TYPE_SET_VARIABLES } from "./constants"
 import SetValuesInspectorComponent from "./SetValuesInspectorComponent.vue"
 import {VisuallyJsService, VisuallyJsServiceKey, InspectorComponent} from "@visuallyjs/browser-ui-vue";
 
-const currentObj = ref<Vertex>(null)
-const currentType = ref('')
+const currentObj = ref<Vertex|null>(null)
+const currentType = computed(() => {
+  if (currentObj.value == null) {
+    return null
+  } else {
+    if (isNode(currentObj.value)) {
+      return currentObj.value.type
+    } else if (isPort(currentObj.value)) {
+      return portTypeMap[currentObj.value.getParent().type]
+    }
+  }
+})
 
 const service:VisuallyJsService = inject(VisuallyJsServiceKey)!
-
-function renderEmptyContainer() {
-  currentType.value = ''
-}
 
 const portTypeMap: Record<string, string> = {
   [TYPE_CONDITIONS]: "condition"
 }
 
-function refresh(obj: Base) {
-  if (isNode(obj)) {
-    currentType.value = obj.type
-  } else if (isPort(obj)) {
-    currentType.value = portTypeMap[obj.getParent().type]
-  }
-  currentObj.value = obj as Vertex
-}
-
 function updateVariables(variables: Array<any>) {
-  service.getModel((model) => {
-    if (model && currentObj.value) {
-      model.updateNode(currentObj.value, { variables })
-      model.clearSelection()
-    }
-  })
+  if (service.model.value && currentObj.value) {
+    service.model.value.updateNode(currentObj.value, { variables })
+    service.model.value.clearSelection()
+  }
 }
 
 function cancel() {
-  service.getModel(model => model.clearSelection())
+  service.model.value?.clearSelection()
 }
 </script>
 
 <template>
-  <InspectorComponent class="vjs-callflow-inspector" :refresh="refresh" :renderEmptyContainer="renderEmptyContainer">
+  <InspectorComponent class="vjs-callflow-inspector" v-model="currentObj">
     <template v-if="currentType === TYPE_PLAY_AUDIO">
       <span>Text:</span>
       <textarea rows="10" cols="10" :vjs-att="PROPERTY_TEXT" vjs-focus="true" placeholder="enter text to speak..." />
